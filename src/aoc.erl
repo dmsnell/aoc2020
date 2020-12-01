e4445c6a64e70ce4adac9abd518122773c311af9
Dennis Snell <dennis.snell@automattic.com>
2020-12-01 01:59:25 -0700

    Create auto-loader/runner

---
+++ src/aoc.erl
@@ -0,0 +1,26 @@
+-module(aoc).
+
+-export([
+    solve/2,
+    solve_all/0
+]).
+
+solve_all() ->
+    Problems = lists:flatten([get_problems(Day) || Day <- get_days()]),
+    [solve(Day, Part) || {Day, Part} <- Problems].
+
+solve(Day, Part) ->
+    InputType = Day:input_type(),
+    InputName = atom_to_list(Day),
+    {USecs, Value} = timer:tc(Day, Part, [get_input(InputName, InputType)]),
+    {Day, Part, Value, {USecs / 1000, ms}}.
+
+get_days() ->
+    [list_to_atom(Module) || {[$d, $a, $y | _] = Module, _, _} <- code:all_available()].
+
+get_input(Name, number_list) ->
+    input:number_lines(Name).
+
+get_problems(Day) ->
+    code:ensure_loaded(Day),
+    [{Day, Part} || Part <- [p1, p2], erlang:function_exported(Day, Part, 1) == true].
--- src/day1.erl
+++ src/day1.erl
@@ -1,16 +1,21 @@
 -module(day1).
 
 -export([
-    part_one/0,
-    part_two/0
+    input_type/0,
+    p1/1,
+    p2/1
 ]).
 
-part_one() ->
-    Entries = input:number_lines("day1"),
+input_type() -> number_list.
+
+-spec p1(Entries :: list(integer())) -> Product :: integer().
+
+p1(Entries) ->
     [Product | _] = [X * Y || X <- Entries, Y <- Entries, X + Y == 2020],
     Product.
 
-part_two() ->
-    Entries = input:number_lines("day1"),
+-spec p2(Entries :: list(integer())) -> Product :: integer().
+
+p2(Entries) ->
     [Product | _] = [X * Y * Z || X <- Entries, Y <- Entries, Z <- Entries, X + Y + Z == 2020],
     Product.
