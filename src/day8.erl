2f155cdb3e1fdbd61dec734a540afc1c0bd13082
Dennis Snell <dennis.snell@automattic.com>
2020-12-08 02:15:13 -0700

    Optimize day 8 computer
     - replace lists and lists:nth with maps (550ms -> 160ms)
     - replace sets with atomics in profiler (160ms -> 120ms)
     - pre-compile code to map vs. on every run (120ms -> 100ms)
     - pass profiler state as function arg vs. updating map (100ms -> 60ms)
     - run computer in-process instead of spawning (40ms -> 17ms)

--- src/day8.erl
+++ src/day8.erl
@@ -2,27 +2,35 @@
 -behavior(aoc).
 
 -export([input_type/0, parse_input/1, p1/1, p2/1]).
--export([run/1, run/2, run/3]).
+-export([run/1, run/2]).
 
 -type opcode() :: acc | jmp | nop.
 -type instruction() :: {opcode(), integer()}.
--type program() :: list(instruction()).
+-type program() :: #{non_neg_integer() => instruction()}.
 
 -record(s, {
-    code = [] :: program(),
-    pc   = 1  :: non_neg_integer(),
-    acc  = 0  :: integer()
+    acc  = 0   :: integer(),
+    code = #{} :: program(),
+    ooc  = 0   :: non_neg_integer(),
+    pc   = 1   :: non_neg_integer()
 }).
 
 -type profiler(ProfilerState) :: {ProfilerState, fun ((ProfilerState, #s{}) -> ProfilerState)}.
+-type runopts(ProfilerState) :: #{
+    isolation => spawned | in_process,
+    profiler  => profiler(ProfilerState),
+    timeout   => non_neg_integer() | infinity
+}.
+
+-export_type([opcode/0, instruction/0, program/0, profiler/1, runopts/1]).
 
 input_type() -> lines.
 
 parse_input(Lines) ->
-    lists:map(fun decode/1, Lines).
+    compile(lists:map(fun decode/1, Lines)).
 
 p1(Program) ->
-    try run(Program, profile_only_once(new)) of
+    try run(Program, #{profiler => profile_only_once(new, maps:size(Program))}) of
         _NotExpected ->
             terminated
     catch
@@ -36,19 +44,19 @@ p2(Program) ->
         {loop_detected, _PC} -> p2(Program, 0)
     end.
 
-p2(Program, PCGuess) when PCGuess < length(Program) ->
+p2(Program, PCGuess) ->
     case p2_run(swap_op(Program, PCGuess)) of
         Acc when is_integer(Acc) ->
             {Acc, {swap, #{
                 pc  => PCGuess,
-                was => lists:nth(PCGuess + 1, Program),
-                now => swap_op(lists:nth(PCGuess + 1, Program))
+                was => maps:get(PCGuess + 1, Program),
+                now => swap_op(maps:get(PCGuess + 1, Program))
             }}};
         {loop_detected, _PC} -> p2(Program, PCGuess + 1)
     end.
 
 p2_run(Program) ->
-    try run(Program, profile_only_once(new), 1000) of
+    try run(Program, #{profiler => profile_only_once(new, maps:size(Program)), timeout => 1000}) of
         {_Pstate, #s{acc = Acc}} -> Acc
     catch
         error:{already_called, #s{pc = PC}} ->
@@ -60,41 +68,50 @@ swap_op({jmp, V}) -> {nop, V};
 swap_op({nop, V}) -> {jmp, V}.
 
 swap_op(Program, PCGuess) ->
-    {Before, [Op | After]} = lists:split(PCGuess, Program),
-    Before ++ [swap_op(Op) | After].
+    maps:update_with(PCGuess + 1, fun swap_op/1, Program).
 
 %% Profilers
 
-profile_only_once(new) ->
-    {sets:new(), fun profile_only_once/2}.
+profile_only_once(new, Length) ->
+    {atomics:new(Length, []), fun profile_only_once/2};
 
 profile_only_once(Called, #s{pc = PC} = State) ->
-    case sets:is_element(PC, Called) of
-        true  -> erlang:error({already_called, State});
-        false -> sets:add_element(PC, Called)
+    case atomics:add_get(Called, PC, 1) of
+        1 -> Called;
+        _ -> erlang:error({already_called, State})
     end.
 
 %% Emulator
 
 run(Program) ->
-    run(Program, 1000).
+    run(Program, #{}).
 
-run(Program, Timeout) when is_integer(Timeout) ->
-    run(Program, {[], fun (_PState, #s{}) -> ok end}, Timeout);
-
-run(Program, {_PState, PFunc} = Profiler) when is_function(PFunc) ->
-    run(Program, Profiler, 1000).
-
-run(Program, {_PState, PFunc} = Profiler, Timeout) when is_list(Program), is_function(PFunc) ->
-    utils:isolated(fun () -> run_loop(Profiler, #s{code = Program}) end, Timeout).
-
-run_loop({PState, _Profiler}, #s{code = Code, pc = PC} = State) when PC > length(Code) ->
-    {PState, State};
-run_loop({PState, Profiler}, #s{code = Code, pc = PC} = State) ->
-    NextPState = Profiler(PState, State),
-    case execute(State, lists:nth(PC, Code)) of
-        {continue, NextState} -> run_loop({NextPState, Profiler}, NextState)
+run(Program, RunOpts) when is_map(RunOpts) ->
+    #{timeout := Timeout} = WithDefaults = maps:merge(#{
+        isolation => in_process,
+        profiler  => {nil, fun (PState, _State) -> PState end},
+        timeout   => 1000
+    }, RunOpts),
+    State = #s{code = Program, ooc = maps:size(Program)},
+    case maps:get(isolation, WithDefaults) of
+        spawned    -> utils:isolated(fun () -> run_loop(WithDefaults, State) end, Timeout);
+        in_process -> run_loop(WithDefaults, State)
     end.
+
+run_loop(#{profiler := {InitialPState, _Profiler}} = RunOpts, State) ->
+    run_loop(RunOpts, State, InitialPState).
+
+run_loop(_RunOpts, #s{ooc = OOC, pc = PC} = State, PState) when PC > OOC ->
+    {PState, State};
+run_loop(#{profiler := {_IPState, Profiler}} = RunOpts, #s{code = Code, pc = PC} = State, PState) ->
+    NextPState = Profiler(PState, State),
+    case execute(State, maps:get(PC, Code)) of
+        {continue, NextState} -> run_loop(RunOpts, NextState, NextPState)
+    end.
+
+compile(Program) when is_list(Program) ->
+    Pairs = lists:zip(lists:seq(1, length(Program)), Program),
+    maps:from_list(Pairs).
 
 execute(#s{acc = Acc, pc = PC} = State, {acc, N}) when is_integer(N) ->
     {continue, State#s{acc = Acc + N, pc = PC + 1}};
